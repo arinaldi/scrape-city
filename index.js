293959ccd3f417cc8402bc8a317dd52dd495486c
Tony Rinaldi <tony.rinaldi@silvercar.com>
2020-04-17 16:56:08 -0500

    Recursive scrape; match artist

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+import artists from './artists';
+
 const BASE_URL = 'http://newalbumreleases.net/category/cat/page';
-const EN_DASH = '&#8211;';
+const artistsLower = artists.map(artist => artist.toLowerCase().replace('&', 'and'));
 
 const getHtml = async (url) => {
   const { data: html } = await axios.get(url);
   return html;
 };
 
-const getReleases = async () => {
-  const html = await getHtml(`${BASE_URL}/1`);
+const checkDate = ($) => {
+  let shouldContinue;
+
+  $('.clock').each((i, el) => {
+    const postDateString = $(el).text();
+    const postDate = parseInt(postDateString.split(' ')[4]);
+    const today = (new Date()).getDate();
+
+    shouldContinue = postDate === today;
+  });
+
+  return shouldContinue;
+};
+
+const getReleases = async (page = 1) => {
+  const html = await getHtml(`${BASE_URL}/${page}`);
   const $ = cheerio.load(html);
   const posts = $('.title > h2 > a')
     .get()
@@ -18,7 +34,26 @@ const getReleases = async () => {
       title: post.attribs.title.replace('Permanent Link to ', ''),
       link: post.attribs.href,
     }));
-  console.log(posts);
+
+  posts.forEach(({ title, link }) => {
+    const enDashIndex = title.indexOf('–');
+    const artist = title
+      .substring(0, enDashIndex)
+      .replace('&', 'and')
+      .toLowerCase()
+      .trim();
+
+    if (artistsLower.includes(artist)) {
+      console.log('TITLE:', title);
+      console.log('LINK:', link);
+    }
+  });
+
+  const shouldContinue = checkDate($);
+
+  if (shouldContinue) {
+    getReleases(page + 1);
+  }
 };
 
 getReleases();
