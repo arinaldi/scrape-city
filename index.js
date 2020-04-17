66bb0eb73b3a35cb23f3ad356f8e15ea8616bf94
Tony <arinaldi@vt.edu>
2020-04-17 18:57:16 -0500

    Log output together

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+// import cron from 'node-cron';
 
 import artists from './artists';
 
 const BASE_URL = 'http://newalbumreleases.net/category/cat/page';
-const artistsLower = artists.map(artist => artist.toLowerCase().replace('&', 'and'));
+const artistsLower = artists.map(artist => (
+  artist.toLowerCase().replace('&', 'and')
+));
 
 const getHtml = async (url) => {
   const { data: html } = await axios.get(url);
@@ -25,6 +28,8 @@ const checkDate = ($) => {
   return shouldContinue;
 };
 
+const results = [];
+
 const getReleases = async (page = 1) => {
   const html = await getHtml(`${BASE_URL}/${page}`);
   const $ = cheerio.load(html);
@@ -35,17 +40,16 @@ const getReleases = async (page = 1) => {
       link: post.attribs.href,
     }));
 
-  posts.forEach(({ title, link }) => {
-    const enDashIndex = title.indexOf('–');
-    const artist = title
+  posts.forEach(post => {
+    const enDashIndex = post.title.indexOf('–');
+    const artist = post.title
       .substring(0, enDashIndex)
       .replace('&', 'and')
       .toLowerCase()
       .trim();
 
     if (artistsLower.includes(artist)) {
-      console.log('TITLE:', title);
-      console.log('LINK:', link);
+      results.push(post);
     }
   });
 
@@ -53,7 +57,17 @@ const getReleases = async (page = 1) => {
 
   if (shouldContinue) {
     getReleases(page + 1);
+  } else if (results.length > 0) {
+    results.forEach(result => {
+      console.log(result.title);
+      console.log(result.link);
+    });
   }
 };
 
 getReleases();
+// 0 8 * * *
+// cron.schedule('* * * * *', () => {
+//   console.log('RUNNING THE CRON');
+//   getReleases();
+// });
